0b40834434349883fc9b1f0ed655a6b535f24449
Mina Malak <mina.malak@nexspec.com>
2026-04-01 21:03:14 +0200

    feat: init table

---
+++ ui/app/layout.tsx
@@ -0,0 +1,24 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+      weight: ['300', '400', '500', '700'],
+      subsets: ['latin'],
+      display: 'swap',
+      variable: '--font-roboto',
+});
+
+export default function RootLayout(props) {
+    const { children } = props;
+    return (
+        <html lang="en" className={roboto.variable}>
+            <body>
+                <AppRouterCacheProvider>
+                       {/*<ThemeProvider theme={theme}>*/}
+                                {children}
+                       {/*</ThemeProvider>*/}
+                </AppRouterCacheProvider>
+            </body>
+        </html>
+    );
+}
