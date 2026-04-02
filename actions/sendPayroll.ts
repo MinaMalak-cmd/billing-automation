f3de4c3123df2d27ff7dc2496943564507f87c27
Mina Malak <mina.malak@nexspec.com>
2026-04-02 19:43:39 +0200

    feat: ignore ts rule

--- actions/sendPayroll.ts
+++ actions/sendPayroll.ts
@@ -1,6 +1,7 @@
 'use server'
 import fs from 'fs/promises';
 import path from 'path';
+// @ts-ignore
 import XlsxPopulate from 'xlsx-populate';
 import nodemailer from 'nodemailer';
 import * as XLSX from 'xlsx';
